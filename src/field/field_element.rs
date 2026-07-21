ac330995cb2c5a46e777156611982fa223c0abdd
DiMatiaz <dimatiaz@gmail.com>
2026-07-21 23:04:08 +0100

    Added algebraic multiplication and division for FieldElement

--- src/field/field_element.rs
+++ src/field/field_element.rs
@@ -1,10 +1,15 @@
-use std::ops::{Add, Sub};
+use std::{
+    fmt::Display,
+    ops::{Add, Div, Mul, Sub},
+};
 
 #[derive(Debug, Copy, Clone, PartialEq)]
 pub struct FieldElement<const P: u64> {
     pub value: u64,
 }
 
+//During initialization we make sure the value falls into (0, p-1) through reaminder or euclidian
+//remainder in case of negative values being a possibility
 impl<const P: u64> FieldElement<P> {
     pub fn from_u64(value: u64) -> Self {
         Self { value: value % P }
@@ -16,6 +21,7 @@ impl<const P: u64> FieldElement<P> {
     }
 }
 
+//Since we create a new FieldElement the range will always fall in (0, P-1)
 impl<const P: u64> Add for FieldElement<P> {
     type Output = Self;
     fn add(self, element: Self) -> Self::Output {
@@ -23,9 +29,78 @@ impl<const P: u64> Add for FieldElement<P> {
     }
 }
 
+//Since we create a new FieldElement the range will always fall in (0, P-1)
 impl<const P: u64> Sub for FieldElement<P> {
     type Output = Self;
     fn sub(self, element: Self) -> Self::Output {
         Self::from_i64((self.value as i64 - element.value as i64) as i64)
     }
+}
+
+//For learning purposes we use u64 and u128 to avoid multiplication overflwow
+impl<const P: u64> Mul for FieldElement<P> {
+    type Output = Self;
+    fn mul(self, element: Self) -> Self::Output {
+        let res = (self.value as u128 * element.value as u128) % P as u128;
+        Self::from_u64(res as u64)
+    }
+}
+
+/// We actually multiply by one of the inverse
+impl<const P: u64> Div for FieldElement<P> {
+    type Output = Self;
+    fn div(self, element: Self) -> Self::Output {
+        if element.value == 0 {
+            Self { value: 0 }
+        } else {
+            match mod_inverse(element.value, P) {
+                Some(x) => {
+                    let res = x * self.value;
+                    return FieldElement::from_u64(res);
+                }
+                None => {
+                    panic!("Error calculating euclidian inverse");
+                }
+            };
+        }
+    }
+}
+
+// Display
+impl<const P: u64> Display for FieldElement<P> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.value)
+    }
+}
+
+/// Extended Euclidian Algorithm
+fn mod_inverse(e: u64, p: u64) -> Option<u64> {
+    //coeficient of e
+    let (mut t, mut new_t) = (0, 1);
+    //GCD(p, e)
+    let (mut r, mut new_r) = (p as i64, e as i64);
+
+    while new_r != 0 {
+        let quotient = r / new_r;
+
+        // Update t
+        let temp_t = t;
+        t = new_t;
+        new_t = temp_t - quotient * new_t;
+
+        // Update r
+        let temp_r = r;
+        r = new_r;
+        new_r = temp_r - quotient * new_r;
+    }
+
+    // If gcd > 1, inverse doesn't exist
+    if r > 1 {
+        return None;
+    }
+
+    if t < 0 {
+        t = t + (p as i64);
+    }
+    Some(t as u64)
 }
--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,34 @@ use crate::field::field_element::FieldElement;
 mod field;
 
 fn main() {
-    let field_element: FieldElement<17> = FieldElement::from_u64(55);
+    //after initialization there are no negative field elements
+    let field_element: FieldElement<17> = FieldElement::from_u64(6);
     let negative_field_element: FieldElement<17> = FieldElement::from_i64(-55);
+
     println!("Hello algebraic element {}", field_element.value);
     println!(
         "Hello negative algebraic element {}",
-        negative_field_element.value
+        negative_field_element
     );
 
     let field_element_operation: FieldElement<17> = field_element + negative_field_element;
-    println!("Hello algebraic element: {}", field_element_operation.value);
+    println!(
+        "Hello algebraic element addition: {}",
+        field_element_operation
+    );
 
     let field_element_subtract: FieldElement<17> = field_element - negative_field_element;
-    println!("Hello algebraic element: {}", field_element_subtract.value);
+    println!(
+        "Hello algebraic element subtracted: {}",
+        field_element_subtract
+    );
+
+    let field_element_multiplied: FieldElement<17> = field_element * negative_field_element;
+    println!(
+        "Hello algebraic element multiplied: {}",
+        field_element_multiplied
+    );
+
+    let field_element_divided: FieldElement<17> = field_element / negative_field_element;
+    println!("Hello algebraic element divided: {}", field_element_divided);
 }
