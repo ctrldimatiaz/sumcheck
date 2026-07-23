b902c42eb34ac0323ebbeaea92598ed4e886602b
DiMatiaz <dimatiaz@gmail.com>
2026-07-23 13:09:01 +0100

    Stop tracking target directory

--- target/debug/deps/sumcheck-6b7862b62047dd6d.d
+++
@@ -1,7 +0,0 @@
-/var/home/dimatiaz/Documents/Projects/Rustlings/sumcheck/target/debug/deps/sumcheck-6b7862b62047dd6d.d: src/main.rs src/field/mod.rs src/field/field_element.rs
-
-/var/home/dimatiaz/Documents/Projects/Rustlings/sumcheck/target/debug/deps/sumcheck-6b7862b62047dd6d: src/main.rs src/field/mod.rs src/field/field_element.rs
-
-src/main.rs:
-src/field/mod.rs:
-src/field/field_element.rs:
